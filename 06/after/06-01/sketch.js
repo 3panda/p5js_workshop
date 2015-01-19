f75d1f2f5c6ef4eaa2b073ae4d7c32cfcee796b0
3panda <3panda@gmail.com>
2015-01-20 08:51:07 +0900

    06 イベントのサンプルの作成準備

---
+++ 06/after/06-01/sketch.js
@@ -0,0 +1,38 @@
+var d;　　　　　 //オブジェクトの直径　Diameter の d
+var r;　　       //オブジェクトの半径  Radius の r
+var x;           //オブジェクトのx座標
+var y;　　　　　 //オブジェクトのy座標
+var xSpeed;　　　//オブジェクトのx座標の移動速度
+var ySpeed;　　　//オブジェクトのy座標の移動速度
+
+function setup() {
+	createCanvas(500, 400);
+	background(51,54,49);
+	fill(204, 101, 192);
+	noStroke();
+
+	//初期化
+	d = 100; 
+	r = d/2;
+	x = r;
+	y = r;
+	xSpeed = 5;
+	ySpeed = 1;
+}
+
+function draw() {
+	background(51,54,49);
+	ellipse(x, y, d, d);
+	x = x + xSpeed;
+	y = y + ySpeed;
+	
+
+	if(x > width - r || x < 0 + r) {
+		xSpeed = -xSpeed;
+	}
+
+	if(y > height - r || y < 0 + r) {
+		ySpeed = -ySpeed;
+	}
+
+}
